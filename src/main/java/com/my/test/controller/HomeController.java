7cd60fc904a97e619b5b9daa192547a27d6a836f
hyunse.lee <hyunse.lee@nhnent.com>
2017-10-13 11:09:40 +0900

    project setting

--- src/main/java/com/my/test/controller/HomeController.java
+++ src/main/java/com/my/test/controller/HomeController.java
@@ -34,16 +34,9 @@ public class HomeController {
 	public String home(Locale locale, Model model) {
 		logger.info("Welcome home! The client locale is {}.", locale);
 		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
 		logger.info("a.name = {}, b.name= {}", aName, bName);
 		logger.info("a.name = {}, b.name= {}", env.getProperty("a.name"), env.getProperty("b.name"));
 		
 		return "home";
 	}
-	
 }
