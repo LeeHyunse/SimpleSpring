4fc5304fbfb467ca0ecc2eeb65e5ed40fa2ed95c
hyunse.lee <hyunse.lee@nhnent.com>
2017-10-13 19:19:32 +0900

    logback

--- src/main/java/com/my/test/config/WebInitializer.java
+++ src/main/java/com/my/test/config/WebInitializer.java
@@ -1,5 +1,8 @@
 package com.my.test.config;
 
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
 public class WebInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
@@ -19,6 +22,11 @@ public class WebInitializer extends AbstractAnnotationConfigDispatcherServletIni
 	@Override
 	protected String[] getServletMappings() {
 		return new String[]{"/"};
+	}
+	
+	@Override
+	final public void onStartup(ServletContext servletContext) throws ServletException {
+		super.onStartup(servletContext);
 	}
 
 }
--- src/main/java/com/my/test/controller/HomeController.java
+++ src/main/java/com/my/test/controller/HomeController.java
@@ -1,14 +1,9 @@
 package com.my.test.controller;
 
-import java.text.DateFormat;
-import java.util.Date;
 import java.util.Locale;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,21 +17,12 @@ public class HomeController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
-	@Value("${a.name}")
-	private String aName;
-	@Value("${b.name}")
-	private String bName;
-	
-	@Autowired
-	private Environment env;
-	
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
-		
-		logger.info("a.name = {}, b.name= {}", aName, bName);
-		logger.info("a.name = {}, b.name= {}", env.getProperty("a.name"), env.getProperty("b.name"));
-		
+		logger.debug("로그출력 테스트 debug");
+		logger.info("로그출력 테스트 info");
+		logger.warn("로그출력 테스트 warn");
+		logger.error("로그출력 테스트 error");
 		return "home";
 	}
 }
